7c9fb5912c3301ec55adfc6cb77f8b2bc502b8c0
Vishal Patel <85049843+venteury@users.noreply.github.com>
2023-04-03 00:53:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000 ; 
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
